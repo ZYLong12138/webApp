3687c3891b7415064efe05cdafe9c1bb476a9092
ZYLong12138 <begin1999@163.co>
2025-03-23 16:31:15 +0800

    更新vocabulay文档中数据结构名称

--- types/vocabulary.ts
+++ types/vocabulary.ts
@@ -1,5 +1,5 @@
 // 单词表数据结构
-export interface Vocabulary {
+export interface VocabularyWord {
   id: number;           // int8 类型
   words: string;        // text 类型
   pronunciation: string; // text 类型
@@ -8,7 +8,7 @@ export interface Vocabulary {
 }
 
 // 新增单词时的数据结构（不包含 id，因为 id 由数据库自动生成）
-export interface NewVocabulary {
+export interface NewVocabularyWord {
   words: string;
   pronunciation: string;
   mainMeaning: number;
